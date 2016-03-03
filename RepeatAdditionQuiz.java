8c4dcf5211aed80825bb1b93e3d7bd0562d12f50
Rosaline24 <rosaline.ascii@gmail.com>
2016-03-03 21:22:36 +0800

    comment
    add comment

--- RepeatAdditionQuiz.java
+++ RepeatAdditionQuiz.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 public class RepeatAdditionQuiz {
 	public static void main(String[] args) {
+	//Create ArryList object
 	ArrayList<Integer> ans = new ArrayList<Integer>();
 	int number1 = (int)(Math.random() * 10);
 	int number2 = (int)(Math.random() * 10);
@@ -13,20 +14,23 @@ public class RepeatAdditionQuiz {
 	System.out.print(
 	"What is " + number1 + " + " + number2 + "? ");
 	int answer = input.nextInt();
+	//add answer into ArrayList
 	ans.add(answer);
 
-
+	//while loop
 	while (number1 + number2 != answer) {
 		System.out.print("Wrong answer. Try again. What is "
 		+ number1 + " + " + number2 + "? ");
 		answer = input.nextInt();
-		
+		//for loop
 		for(int i = 0;i < ans.size(); i++){
+			//if answers are same, print out
 			if(ans.get(i)==answer){
 				System.out.println("You've already entered:" + ans.get(i));
 				break;
 			}
 		}
+		//add answer into ArrayList
 		ans.add(answer);
 	}
 
